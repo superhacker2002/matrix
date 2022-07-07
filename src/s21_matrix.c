7e6b3b90ab176dc38d8c4b2209347dacc5bbc403
superhacker2002 <margo.gamaleeva@yandex.ru>
2022-07-07 15:10:26 +0300

    equality

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -1,36 +1,39 @@
 #include "s21_matrix.h"
 
 int main() {
-    matrix_t matrix;
-    matrix_init(&matrix);
-    s21_create_matrix(3, 4, &matrix);
-    s21_fill_matrix(&matrix);
-    s21_print_matrix(&matrix);
-
-    printf("%d\n", matrix.rows);
-    printf("%d\n", matrix.columns);
-    s21_remove_matrix(&matrix);
+    matrix_t s21_matrix;
+    matrix_t s21_matrix_2;
+    s21_create_matrix(3, 4, &s21_matrix);
+    s21_create_matrix(3, 4, &s21_matrix_2);
+    s21_fill_matrix(&s21_matrix, 1.1234567);
+    s21_print_matrix(s21_matrix);
+    
+    s21_fill_matrix(&s21_matrix_2, 1.1234567);
+    s21_print_matrix(s21_matrix_2);
+    // if (s21_eq_matrix(&s21_matrix, &s21_matrix_2) == SUCCESS) {
+    //     printf("equal\n");
+    // } else {
+    //     printf("not equal\n ");
+    // }
+    // printf("%d\n", matrix.rows);
+    // printf("%d\n", matrix.columns);
+    s21_remove_matrix(&s21_matrix);
+    s21_remove_matrix(&s21_matrix_2);
 }
 
-void matrix_init(matrix_t *A) {
-    A->columns = 0;
-    A->rows = 0;
-    A->matrix = NULL;
-}
-
-void s21_print_matrix(matrix_t *A) {
-    for (int row; row < A->rows; row++) {
-        for (int column; column < A->columns; column++) {
-            printf("%f ", A->matrix[row][column]);
+void s21_print_matrix(matrix_t A) {
+    for (int row = 0; row < A.rows; row++) {
+        for (int column = 0; column < A.columns; column++) {
+            printf("%.10lf ", A.matrix[row][column]);
         }
         printf("\n");
     }
 }
 
-void s21_fill_matrix(matrix_t *A) {
-    for (int row; row < A->rows; row++) {
-        for (int column; column < A->columns; column++) {
-            A->matrix[row][column] = 1.12345678; 
+void s21_fill_matrix(matrix_t *A, double value) {
+    for (int row = 0; row < A->rows; row++) {
+        for (int column = 0; column < A->columns; column++) {
+            A->matrix[row][column] = value; 
         }
     }
 }
@@ -42,9 +45,9 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
     } else {
         result->rows = rows;
         result->columns = columns;
-        result->matrix = (double **)malloc(rows * sizeof(double*));
+        result->matrix = calloc(rows, sizeof(double*));
         for (int row = 0; row < rows; row++) {
-           result->matrix[row] = (double *)malloc(columns * sizeof(double));
+           result->matrix[row] = calloc(columns, sizeof(double));
         }
     }
     return status;
@@ -54,23 +57,24 @@ void s21_remove_matrix(matrix_t *A) {
     for (int row = 0; row < A->rows; row++) {
         free(A->matrix[row]);
     }
-    free(A);
+    free(A->matrix);
 }
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
     int status = SUCCESS;
-    double precision = 0.0000001;
+    double eps = 0.0000001;
     if (A->rows == B->rows && A->columns == B->columns) {
-        int equal = 0;
-        status = FAILURE;
-        for (int row; row < A->rows; row++) {
-            for (int column; column < A->columns; column++) {
-                if ((A->matrix[row][column] - precision) < B->matrix[row][column] &&
-                    (A->matrix[row][column] + precision) > B->matrix[row][column]) {
+        for (int row = 0; row < A->rows; row++) {
+            for (int column = 0; column < A->columns; column++) {
+                if (fabs(A->matrix[row][column] - B->matrix[row][column]) < eps) { 
+                } else {
                     status = FAILURE;
-                    break;  
+                    break; 
                 }
             }
         }
+    } else {
+        status = FAILURE;
     }
+    return status;
 }
--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -2,6 +2,7 @@
 #define SRC_S21_MATRIX_H_
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #define SUCCESS 1
 #define FAILURE 0
 
@@ -14,9 +15,8 @@ typedef struct matrix_struct {
 int s21_create_matrix(int rows, int columns, matrix_t *result);
 void s21_remove_matrix(matrix_t *A);
 int s21_eq_matrix(matrix_t *A, matrix_t *B);
-void s21_print_matrix(matrix_t *A);
-void s21_fill_matrix(matrix_t *A);
-void s21_print_matrix(matrix_t *A);
+void s21_print_matrix(matrix_t A);
+void s21_fill_matrix(matrix_t *A, double value);
 void matrix_init(matrix_t *A);
 
 #endif // SRC_S21_MATRIX_H_
